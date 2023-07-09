657b223e03628b560c6e6c092830bf799c048748
rakr0819 <raman.krauchanka@netcracker.com>
2023-07-09 20:07:56 +0300

    next app example

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts asd', {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
         next: {
             revalidate: 60,
         }
